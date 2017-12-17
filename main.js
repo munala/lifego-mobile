0e9c129681460d811c5626c73b05e6cfe34e622f
Oliver Munala <munalaoliverngoitsi@gmail.com>
2017-12-17 18:25:59 +0300

    chore(store): add redux persist

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 import { Provider } from 'react-redux';
 import React from 'react';
+import { PersistGate } from 'redux-persist/es/integration/react';
 
 import configureStore from './store/configureStore';
 import App from './App';
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 export default () => (
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- store/configureStore.js
+++ store/configureStore.js
@@ -1,12 +1,23 @@
 import { createStore, applyMiddleware } from 'redux';
+import { AsyncStorage } from 'react-native';
+import { persistStore, persistReducer } from 'redux-persist';
+import logger from 'redux-logger';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-export default function configureStore(initialState) {
-  return createStore(
-    rootReducer,
+const configureStore = (initialState) => {
+  const config = {
+    key: 'app',
+    storage: AsyncStorage,
+  };
+  const store = createStore(
+    persistReducer(config, rootReducer),
     initialState,
-    applyMiddleware(thunk, reduxImmutableStateInvariant()),
+    applyMiddleware(thunk, reduxImmutableStateInvariant(), logger),
   );
-}
+  const persistor = persistStore(store);
+  return { store, persistor };
+};
+
+export default configureStore;
