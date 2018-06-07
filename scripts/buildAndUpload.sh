9b6524285f34d124b446f137fe7fa8fdf3d8f99d
Oliver Munala <munalaoliverngoitsi@gmail.com>
2018-06-07 11:57:57 +0300

    fix(build): fix build script

--- scripts/buildAndUpload.sh
+++ scripts/buildAndUpload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd android && ./gradlew assembleRelease && cd .. && /Users/olivermunala/bin/dbxcli rm app-release.apk && /Users/olivermunala/bin/dbxcli put /Users/olivermunala/dev/bucketlist-react-native/BucketListNative/src/android/app/build/outputs/apk/release/app-release.apk app-release.apk
+cd android && ./gradlew assembleRelease && cd .. && /Users/olivermunala/bin/dbxcli rm app-release.apk && /Users/olivermunala/bin/dbxcli put /Users/olivermunala/dev/bucketlist-react-native/BucketListNative/android/app/build/outputs/apk/release/app-release.apk app-release.apk
