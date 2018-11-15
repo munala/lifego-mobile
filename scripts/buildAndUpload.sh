2622fb0646dcf4312786da133c80e79c78193923
Oliver Munala <munalaoliverngoitsi@gmail.com>
2018-11-15 16:23:56 +0300

    chore(build): change build script

--- scripts/buildAndUpload.sh
+++ scripts/buildAndUpload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd android && ./gradlew assembleRelease && cd .. && /Users/olivermunala1/bin/dbxcli rm lifego/app-release.apk && /Users/olivermunala1/bin/dbxcli put /Users/olivermunala1/dev/bucketlist-react-native/BucketListNative/android/app/build/outputs/apk/release/app-release.apk lifego/app-release.apk
+cd android && ./gradlew assembleRelease && cd .. && dbxcli rm lifego/app-release.apk && dbxcli put ./android/app/build/outputs/apk/release/app-release.apk lifego/app-release.apk
